fe7962d5ab2ea0d44d75fdd248b92c782d571b0c
epovv <epov.vasilii@gmail.com>
2019-07-01 00:18:38 +1000

    Еще тест веток

--- lecturer/views.py
+++ lecturer/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import Http404, HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
