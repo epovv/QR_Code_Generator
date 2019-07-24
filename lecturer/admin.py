14c8d64c2060d166770a5801736a2f7ce9462ca4
epovv <epov.vasilii@gmail.com>
2019-07-24 15:21:27 +1000

    new models + functional

--- lecturer/admin.py
+++ lecturer/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from lecturer.models import *
-# from . import random_generator
+from django.contrib.auth import models
 
 
 class StudentsAllAdmin(admin.ModelAdmin):
@@ -8,25 +8,68 @@ class StudentsAllAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Имя студента:', {'fields': ['name']}),
         ('Группа:', {'fields': ['my_group']}),
-        ('Активность:', {'fields': ['activity']})
+        ('Дата зачисления:', {'fields': ['start_date']}),
+        ('Активность:', {'fields': ['activity']}),
+        ('Комментарий:', {'fields': ['comment']}),
     ]
 
-    list_display = ('name', 'activity', 'my_group')
+    list_display = ('name', 'activity', 'start_date')
+    list_filter = ('my_group', 'activity', 'start_date')
+
+
+class StudentsAllInLines(admin.TabularInline):
+    """Для добавления новых студентов прямо в группах"""
+    model = StudentsAll.my_group.through
+    extra = 2
 
 
 class LectureAdmin(admin.ModelAdmin):
     """Описание модели Lecture в админке"""
     fieldsets = [
-        ('Название лекции', {'fields': ['lecture_name']}),
-        ('Дата', {'fields': ['time']}),
-        ('Группы присутствующие на лекции', {'fields': ['group']})
+        ('Название лекции:', {'fields': ['lecture_name']}),
+        ('Дата:', {'fields': ['time']}),
+        ('Группы присутствующие на лекции:', {'fields': ['group']}),
+        ('Комментарий:', {'fields': ['comment']})
     ]
 
     list_display = ('lecture_name', 'time', 'stud_count_admin')
+    list_filter = ('time', 'group')
 
+
+class GroupAdmin(admin.ModelAdmin):
+    """Описание модели Group в админке"""
+    list_display = (
+        'group_name',
+        'course',
+        'start_date',
+        'end_date',
+        'stud_count_admin',
+        'activity'
+    )
+    list_filter = ('course', 'start_date', 'activity')
+    inlines = (
+        StudentsAllInLines,
+    )
+
+
+class GroupInLines(admin.StackedInline):
+    """Для создания новых групп прямо в курсах"""
+    model = Group
+    extra = 0
+
+class CourseAdmin(admin.ModelAdmin):
+    """Описание модели Course в админке"""
+    list_display = ('course_name', 'activity')
+    list_filter = ('activity', )
+
+    inlines = (
+        GroupInLines,
+    )
 
 admin.site.register(StudentsAll, StudentsAllAdmin)
 admin.site.register(Lecture, LectureAdmin)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Course, CourseAdmin)
 
-# random_generator.run_random()
+admin.site.unregister(models.Group)
+admin.site.unregister(models.User)
--- lecturer/urls.py
+++ lecturer/urls.py
@@ -5,10 +5,10 @@ from .views import *
 urlpatterns = [
     path('', main_page, name='main_page_url'),
     path('create_lecture/', receiving_data, name='receiving_data_url'),
-    path('registration/', register, name='registration_url'),
     path('qr/<int:id>/', qr_generator, name='qr_generator_url'),
-    path('qr/<int:id>/check_your_self/', check, name='check_your_self_url'),
+    path('qr/<int:id>/check/', check, name='check_your_self_url'),
     path('logout/', logout_view, name='logout'),
     path('lectures/', lecture, name='lecture'),
     path('students/', student, name='student'),
+    path('lectures/<int:id>', lecture_more, name='lecture_more_url'),
 ]
